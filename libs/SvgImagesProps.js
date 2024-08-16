52a30dc3c22747f2409d78cf300d6672c8984e91
kusariya <yusaki.4kusari@gmail.com>
2024-08-16 11:31:31 +0900

    Pathとrefreshパラメータを追加

--- libs/SvgImagesProps.js
+++ libs/SvgImagesProps.js
@@ -10,7 +10,9 @@ class SvgImageProps{
      * 
      */
 
-    constructor(){
+    constructor(documentPath){
+        this.Path = documentPath;
+        this.refresh = []; //これがデフォルトを設定すべきなのか不明
         this.altdMap = new WeakMap();
         this.styleMap = new WeakMap();
         this.CRS = new Map();
